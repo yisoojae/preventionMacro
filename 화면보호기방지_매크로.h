ddd37f3d6cdf4783da41a58223ce192b35354b2c
ysj <captnwo@naver.com>
2023-06-19 15:57:05 +0900

    init

---
+++ 화면보호기방지_매크로.h
@@ -0,0 +1,32 @@
+﻿
+// 화면보호기방지_매크로.h: PROJECT_NAME 애플리케이션에 대한 주 헤더 파일입니다.
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH에 대해 이 파일을 포함하기 전에 'pch.h'를 포함합니다."
+#endif
+
+#include "resource.h"		// 주 기호입니다.
+
+
+// C화면보호기방지매크로App:
+// 이 클래스의 구현에 대해서는 화면보호기방지_매크로.cpp을(를) 참조하세요.
+//
+
+class C화면보호기방지매크로App : public CWinApp
+{
+public:
+	C화면보호기방지매크로App();
+
+// 재정의입니다.
+public:
+	virtual BOOL InitInstance();
+
+// 구현입니다.
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern C화면보호기방지매크로App theApp;
---
+++ 화면보호기방지_매크로Dlg.h
@@ -0,0 +1,34 @@
+﻿
+// 화면보호기방지_매크로Dlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// C화면보호기방지매크로Dlg 대화 상자
+class C화면보호기방지매크로Dlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	C화면보호기방지매크로Dlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD___DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
